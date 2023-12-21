c2f04895bcd1dc2376c5983ce0e5b879fb53ab73
devsomsak <devrleang@gmail.com>
2023-12-21 12:16:57 +0700

    feat:solve-HH-State-Greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingText, setGreetingText] = useState("");
+
+  const handleClickSawadee = () => {
+    setGreetingText("สวัสดี!");
+  };
+
+  const handleClickHi = () => {
+    setGreetingText("Hi!");
+  };
+
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
-      <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+      <div className="greeting-container">{greetingText}</div>
+        <div className="buttons">
+        <button onClick={handleClickSawadee}>สวัสดี!</button>
+        <button onClick={handleClickHi}>Hi!</button>
+        <button onClick={()=>{
+             setGreetingText("你好!");
+        }}>你好!</button>
       </div>
     </div>
   );
